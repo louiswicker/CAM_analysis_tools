3b7c4cf4733664e6ec737b39a040fdb7b50b4008
Louis <Wicker>
2021-12-22 14:31:54 -0600

    Remove some superfluous lines.

--- spectra/setup.py
+++ spectra/setup.py
@@ -4,9 +4,6 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
-#include_dirs=[np.get_include()])
-#   extra_compile_args=['-I/Users/jeremy.gibbs/opt/anaconda3/lib/python3.8/site-packages/numpy/core/include'],)]
-
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("spectra", ["spectra.pyx"],
