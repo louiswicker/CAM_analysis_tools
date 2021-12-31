Fixed bug in spectral2d_avg, so that dimension averaging is occuring AFTER the square of fourier amplitudes.  This makes the results look more like the radial averaging.  Also changed spectra3D to spectraND, so that one can now get spectra from arrays having more than 3 dimensions. Spectra are calculated over last two indices and then averaged - so that one can have N hours of data, k-levels of data, and then get a single spectra averaged over hours and levels for the 2D plane.

spectra/py_spectra.py
@@ -6,11 +6,10 @@ from matplotlib import colors, ticker
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 
-
 #-------------------------------------------------------------------------------------
 # 2D Spectra
 
-def get_spectra2d_rad(fld, **kwargs):
+def get_spectra2d_rad(fld, print_info=True, **kwargs):
     """
     Returns 1D power spectra from a 2D field where 2D spectrum is averaged into radial bins.
     There are several caveats.  First, the shape of the fld array must be square.  If it is not square, 
@@ -28,15 +27,17 @@ def get_spectra2d_rad(fld, **kwargs):
               waven:  wavenumber (0 - 1) in non-dimensional space.
     """
 
-    print("\n------------------------")
-    print("get_spectra2d_rad called\n")
+    if print_info:
+        print("\n------------------------")
+        print("get_spectra2d_rad called\n")
 
     ny, nx = fld.shape
         
     if nx != ny:
         
-        print("get_spectra2d_rad: can only analyze process same wavenumbers in X & Y, nx: %d  ny: %d\n" %(nx,ny))
-        print("get_spectra2d_rad: will sample a square domain using nx/2, ny/2 center point\n")
+        if print_info:
+            print("get_spectra2d_rad: can only analyze process same wavenumbers in X & Y, nx: %d  ny: %d\n" %(nx,ny))
+            print("get_spectra2d_rad: will sample a square domain using nx/2, ny/2 center point\n")
         
         if nx > ny:
             nny = int(ny//2) 
@@ -57,7 +58,8 @@ def get_spectra2d_rad(fld, **kwargs):
     ny = 2*(ny//2)
     fourier_image = np.fft.fftn(fld2[0:ny,0:nx])
         
-    print("get_spectra2dr1: Field has [even] dimensions nx: %d  ny: %d\n" %(nx,ny))
+    if print_info: 
+        print("get_spectra2dr1: Field has [even] dimensions nx: %d  ny: %d\n" %(nx,ny))
             
     fourier_amplitudes = np.abs(fourier_image)**2
 
@@ -75,42 +77,16 @@ def get_spectra2d_rad(fld, **kwargs):
     PSbins *= np.pi * (kbins[1:]**2 - kbins[:-1]**2)
     wavenumber = 2*(kvals-1)/nx
     
-    print("------------------------\n")
+    if print_info:
+            print("------------------------\n")
     
     return kvals, PSbins, wavenumber
 
-#-------------------------------------------------------------------------------------
-# 3D Spectra
-
-def get_spectra3d(fld, func = get_spectra2d_rad, **kwargs):
-    """
-    Returns average spectra from 3D data set where the power spectra is averaged along the
-    first dimension.
-    """
-    print("\n----------------------")
-    print("get_spectra3d called")
-    print("----------------------\n")
-
-    
-    if len(fld.shape) != 3:
-        print("get_spectra3d:  Array is wrong size, must have 3 dimensions\n")
-        return None
-    else:
-        
-        Abins = []
-        
-        for k in np.arange(fld.shape[0]):
-            kvals, A, waven = func(fld[k], func=func, **kwargs)
-            Abins.append(A)
-            
-        Abins = np.asarray(Abins)
-        
-        return kvals, Abins.mean(axis=0), waven
 
 #-------------------------------------------------------------------------------------
 # 1D Spectra
 
-def get_spectra2d_avg(fld, axis=1, **kwargs):
+def get_spectra2d_avg(fld, axis=1, print_info=True, **kwargs):
     """
     Returns the average power spectra from a 2D field along one dimension averaging over the second direction.
     
@@ -120,19 +96,30 @@ def get_spectra2d_avg(fld, axis=1, **kwargs):
               PSbins: power spectra which has been binned into kbins
               waven:  wavenumber (0 - 1) in non-dimensional space.
     """
-    print("\n------------------------")
-    print("get_spectra2d_avg called")
-    print("------------------------\n")
+    
+    if print_info: 
+        print("\n------------------------")
+        print("get_spectra2d_avg called")
+        print("------------------------\n")
 
-    nx = fld.shape[axis]
+    # now need to make the number of points even...
+
+    ny, nx = fld.shape
+    nx     = 2*(nx//2)
+    ny     = 2*(ny//2)   
+    fld2   = fld[0:ny,0:nx].copy()
+    
+    # Now pick an axis to average over
+
+    nx = fld2.shape[axis]
     
     other_axis = [1,0]
 
     avg_axis = other_axis[axis]
     
-    fourier_image = np.fft.fft(fld, axis = axis).mean(axis = avg_axis)
+    fourier_image = np.fft.fft(fld2, axis = axis)
     
-    fourier_amplitudes = np.abs(fourier_image)**2
+    fourier_amplitudes = (np.abs(fourier_image)**2).mean(axis = avg_axis)
     
     kfreq   = np.fft.fftfreq(nx) * nx
     
@@ -145,6 +132,49 @@ def get_spectra2d_avg(fld, axis=1, **kwargs):
     wavenumber = 2*(kvals-1)/nx
     
     return kvals, PSbins, wavenumber
+
+#-------------------------------------------------------------------------------------
+# 3D Spectra
+
+def get_spectraND(fld, func = get_spectra2d_rad, **kwargs):
+    """
+    Returns average spectra from ND data set where the power spectra computed along
+    the last two dimensions (often assumed to be x & y).  The input array can have
+    3, 4, or even 5 dimensions (e.g., 5D => [case_day, fhours, klevels, ny, nx]
+    and the function will reshape the input array to have 3 dimensions and then
+    compute spectra over the last two dimensions using the 2D function passed
+    and return the spectra from the last two dimensions.  Normally the spectra is averaged,
+    over the first dimensions, but one can return the 3D array of spectra. 
+    """
+    print("\n----------------------")
+    print("get_spectraND called")
+    print("----------------------\n")
+
+    
+    if fld.ndim < 3:
+        print("get_spectraND:  Array is wrong size, input array must have at least 3 dimensions\n")
+        return None
+    
+    if fld.ndim > 2:
+        
+        fshape = fld.shape
+        start = 0
+        count = fld.ndim-2
+
+        fld3d = np.reshape(fld.copy(), fshape[:start] + (-1,) + fshape[start+count:])
+        
+        if fld.ndim > 3:
+            print("get_spectraND:  Reshaped array so that out dimension is %d\n" % fld3d.shape[0])
+
+        Abins = []
+        
+        for k in np.arange(fld3d.shape[0]):
+            kvals, A, waven = func(fld3d[k], func=func, print_info=False, **kwargs)
+            Abins.append(A)
+            
+        Abins = np.asarray(Abins)
+        
+        return kvals, Abins.mean(axis=0), waven
 #-------------------------------------------------------------------------------------
 # Plot spectral
 
@@ -162,7 +192,7 @@ def plot_spectra(fld, func = get_spectra2d_rad, title = None, ax = None, PScolor
         kvals, Abins, waven = func(fld, **kwargs)
         
     else:
-        kvals, Abins, waven = get_spectra3d(fld, func = func, **kwargs)
+        kvals, Abins, waven = get_spectraND(fld, func = func, **kwargs)
         
     
     if title == None:
@@ -181,7 +211,7 @@ def plot_spectra(fld, func = get_spectra2d_rad, title = None, ax = None, PScolor
         axes[0].loglog(waven, Abins, color=PScolor)
         axes[0].set_xlim(2.0/waven.shape[0], 1.0)
 
-        axes[0].annotate("%s\nLog Power Scale" % title, xy=(0.10, 0.75), xycoords='axes fraction', color='k',fontsize=18)
+        axes[0].annotate("%s\nLog Power Scale" % title, xy=(0.10, 0.25), xycoords='axes fraction', color='k',fontsize=18)
         axes[0].xaxis.set_major_formatter(mticker.FuncFormatter(update_ticks))
         
         
@@ -208,7 +238,7 @@ def plot_spectra(fld, func = get_spectra2d_rad, title = None, ax = None, PScolor
             axes[0].annotate(r"%d" % int(w), xy = (2.0/w-0.01, -0.035), xycoords='axes fraction', color='k',fontsize=12)
             axes[0].axvline(x = 2.0/w-0.0075, color = 'grey', label = 'axvline - full height')
             
-        axes[0].annotate("%s\nLinear Power Scale" % title, xy=(0.70, 0.75), xycoords='axes fraction', color='k',fontsize=18)
+        axes[0].annotate("%s\nLinear Power Scale" % title, xy=(0.70, 0.25), xycoords='axes fraction', color='k',fontsize=18)
 
     axes[0].set_xlabel(r"Wavelength in ($\Delta$x)", fontsize=16)
     