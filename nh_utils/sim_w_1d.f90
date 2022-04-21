3625aeda6c50929c1471e9db2f22c0aa80d99bfc
louis.wicker <Louis.Wicker@noaa.gov>
2022-04-20 19:30:19 -0500

    Improved the tridiagonal algorithm using the Wikipedia.

--- nh_utils/sim_w_1d.f90
+++ nh_utils/sim_w_1d.f90
@@ -1,13 +1,13 @@
 
-!>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+!>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, dz2, pt2, ws, p_fac)
                         
    integer,       intent(in)  :: km
    real(kind=4),  intent(in)  :: dt, rgas, gama, kappa, p_fac
    real(kind=4),  intent(in), dimension(km):: dm2, pt2, pm2, gm2, cp2
-   real(kind=4),  intent(in ) ::  ws
+   real(kind=4),  intent(in ) :: ws
    real(kind=4),  intent(in ), dimension(km+1):: pem
-   real(kind=4),  intent(inout) ::  pe(km+1), dz2(km), w2(km)
+   real(kind=4),  intent(inout) :: pe(km+1), dz2(km), w2(km)
    
    !f2py intent(overwrite) :: pe, dz2, w2
 
@@ -24,7 +24,7 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
 
    do k = 1,km
     
-     pe(k)   = exp(gama*log(-dm2(k)/dz2(k)*rgas*pt2(k))) - pm2(k)        
+     pp(k)   = exp(gama*log(-dm2(k)/dz2(k)*rgas*pt2(k))) - pm2(k)        
      dm2e(k) = dm2(k)
     
      w1(k)   = w2(k)
@@ -36,9 +36,9 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
     
 ! Set up tridiagonal coeffs for spline interpolation of w to grid edges. (Extra copy of bb for end calcs)
 
-    do k = 1,km-1
+    do k = 2,km
        
-      g_rat(k) = dm2e(k)/dm2e(k+1)
+      g_rat(k) = dm2e(k-1)/dm2e(k)
     
       aa(k)    = 1.0
       bb(k)    = 2.*(1.+g_rat(k))
@@ -49,31 +49,31 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
     
 ! Boundary conditions for wE = 0 at top
 
-    bet    = bb(1)
     wE(1)  = 0.0
-    wE(2)  = dd(1) / bet
+    wE(km+1) = 0.0
+
     bb(km) = 2.
     dd(km) = 3.*w1(km)
        
-! Forward calculation of tri-diagonal system
+! Forward calculation of tri-diagonal system  # VBA algorithm from Wikipedia
 
-    do k = 2, km
+    do k = 3, km
     
-      gam(k)  =  g_rat(k-1) / bet
-      bet     =  bb(k) - gam(k)
-      wE(k+1) = (dd(k) - aa(k)*wE(k) ) / bet
+      bet   = aa(k) / bb(k-1)
+      bb(k) = bb(k) - bet * cc(k-1)
+      dd(k) = dd(k) - bet * dd(k-1)
          
     enddo
 
-! Boundary conditions for wE = [ws(i) at ground]  
-  
-      wE(km+1) = 0.0
+! Solve for the last value of matrix
+        
+      wE(km)   = dd(km) / bb(km)
 
 ! Do the back substition, result is wE on zone edges.
 
-    do k = km, 2, -1
+    do k = km-1, 2, -1
 
-      wE(k) = wE(k) - gam(k)*wE(k+1)
+      wE(k) = (dd(k) - cc(k) * wE(k+1)) / bb(k)
         
     enddo
     
@@ -81,7 +81,7 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
 
     do k = 1, km
     
-      aa(k) = dt * gama * (pm2(k)+pe(k)) / dz1(k) 
+      Ak(k) = dt * gama * (pm2(k)+pp(k)) / dz1(k) 
         
     enddo
 
@@ -89,58 +89,51 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
 
     do k = 2, km
 
-      cc(k) = 2.0 * dt / (dm2(k) + dm2(k-1))
-        
-      Bk(k) = 1.0 - cc(k) * (aa(k-1) + aa(k))
+      Ck(k) = 2.0 * dt / (dm2(k) + dm2(k-1))
     
-      Ak(k) = aa(k-1)*cc(k)
+      aa(k) = Ak(k-1)*Ck(k)
         
-      Ck(k) = aa(k  )*cc(k)
+      cc(k) = Ak(k  )*Ck(k)
     
-      Rk(k) = wE(k) + cc(k) * (pe(k) - pe(k-1))
+      dd(k) = wE(k) + Ck(k) * (pp(k) - pp(k-1))
+        
+      bb(k) = 1.0 - cc(k) - aa(k)
 
     enddo    
 
 ! Boundary value calc for forward tri-diagonal solution
 
-   Ck(1)  = 0.0
-
-   Rk(2)  = Rk(2)  - 0.0 * Ck(2)    ! this includes the lower bc for w zero here
+   dd(2)  = dd(2)  - 0.0 * Ck(2)    ! this includes the lower bc for w zero here
     
-   Rk(km) = Rk(km) - 0.0 * Ck(km)   ! this includes the lower bc for ws  (zero here)
+   dd(km) = dd(km) - 0.0 * Ck(km)   ! this includes the lower bc for ws  (zero here)
   
-! Forward sweep.
+! Forward calculation of tri-diagonal system  # VBA algorithm from Wikipedia
 
-    bet   = Bk(2)
-
-    wE(1) = 0.0
-        
-    do k = 2, km
-
-      gam(k) = Ck(k-1) / bet
-        
-      bet    = Bk(k) - Ak(k) * gam(k)
-
-      wE(k)  = (Rk(k) - Ak(k)*wE(k-1) ) / bet
-
-    enddo
+    do k = 3, km
     
-    wE(km+1)  = 0.0
-    gam(km+1) = 0.0
-            
-! Back substitution for solution (wE = 0 at k=1)
+      bet   = aa(k) / bb(k-1)
+      bb(k) = bb(k) - bet * cc(k-1)
+      dd(k) = dd(k) - bet * dd(k-1)
+         
+    enddo
+
+! Solve for the last value of matrix
+        
+      wE(km)   = dd(km) / bb(km)
+        
+! Do the back substition, result is wE on zone edges.
 
     do k = km-1, 2, -1
-      
-      wE(k) = wE(k) - gam(k+1)*wE(k+1)
-            
+
+      wE(k) = (dd(k) - cc(k) * wE(k+1)) / bb(k)
+        
     enddo
         
 ! Solve for new perturbation pressure
 
     do k = 1, km
 
-      pp(k)  = pe(k) - aa(k) * (wE(k+1) - wE(k))
+      pp(k)  = pp(k) - Ak(k) * (wE(k+1) - wE(k))
 
     enddo
     
@@ -161,15 +154,62 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
         
     enddo
     
+! Set up tridiagonal coeffs for spline interpolation of pe to grid edges.
+
+    do k = 2,km
+       
+      g_rat(k) = dm2e(k-1)/dm2e(k)
+    
+      aa(k)    = 1.0
+      bb(k)    = 2.*(1.+g_rat(k))
+      cc(k)    = g_rat(k)
+      dd(k)    = 3.*(pp(k) + cc(k)*pp(k+1))
+
+    enddo
+    
+! Boundary conditions for wE = 0 at top
+
+    g_rat(km+1) = 1.0
+   
+    aa(km+1) = 1.0
+    bb(km+1) = 4.0
+    cc(km+1) = 1.0
+    
+    pe(1) = 0.0
+    
+!    bb(km+1) = 2.
+    dd(km+1) = 3.*pp(km)
+       
+! Forward calculation of tri-diagonal system  # VBA algorithm from Wikipedia
+
+    do k = 3, km+1
+    
+      bet   = aa(k) / bb(k-1)
+      bb(k) = bb(k) - bet * cc(k-1)
+      dd(k) = dd(k) - bet * dd(k-1)
+         
+    enddo
+
+! Solve for the last value of matrix
+        
+      pe(km)   = dd(km) / bb(km)
+
+! Do the back substition, result is wE on zone edges.
+
+    do k = km-1, 2, -1
+
+      pe(k) = (dd(k) - cc(k) * pe(k+1)) / bb(k)
+
+    enddo
+        
+    
 ! Retrieve edge pressures
 
-   pe(1) = 0.0
+!  pe(1) = 0.0
 
-    do k = 1,km
+ !   do k = 1,km
       
-      !pe(k+1) = pe(k) + dm2(k)*(w2(k) - w1(k))*rdt
-      pe(k+1) = pe(k) + pp(k)
-        
-    enddo
+ !     
+ !   enddo
     
  end subroutine SIM_W_1D
