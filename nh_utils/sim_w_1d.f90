07e9ad74d310b470d15b9602b11c42a905c1f244
louis.wicker <Louis.Wicker@noaa.gov>
2022-05-02 14:47:22 -0500

    Friday 29 April changes

--- nh_utils/sim_w_1d.f90
+++ nh_utils/sim_w_1d.f90
@@ -1,8 +1,8 @@
 
 !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, dz2, pt2, ws, p_fac)
+subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, dz2, pt2, ws, p_fac, pe_compute)
                         
-   integer,       intent(in)  :: km
+   integer,       intent(in)  :: km, pe_compute
    real(kind=4),  intent(in)  :: dt, rgas, gama, kappa, p_fac
    real(kind=4),  intent(in), dimension(km):: dm2, pt2, pm2, gm2, cp2
    real(kind=4),  intent(in ) :: ws
@@ -15,10 +15,13 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
 
    real(kind=4), dimension(km  ):: Ak, Bk, Rk, Ck, g_rat, bb, dd, aa, cc, dz1, w1
    real(kind=4), dimension(km+1):: pp, gam, wE, dm2e, wES
-   real  rdt, capa1, bet
+   real  rdt, capa1, bet, r3
+    
+   real :: dwup, dwcn, dwdn, dmtot, wup, wdn, wcn, dzup, dzdn, dzcn, ddmup, ddmcn, ddmdn
 
    rdt   = 1. / dt
    capa1 = kappa - 1.
+   r3    = 1./3.
 
 ! Compute non-hydrostatic pert pressure
 
@@ -47,69 +50,21 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
 
     enddo
     
-! Boundary conditions for wE = 0 at top
+! Boundary conditions for von Neuman at the top
 
-    wE(1)  = 0.0
-    wE(km+1) = 0.0
+    bb(1) = 2.0
+    cc(1) = 1.0
+    dd(1) = 3.0*w1(1)
+
+! Boundary conditions at the bottom...
 
     bb(km) = 2.
     dd(km) = 3.*w1(km)
-       
+    wE(km+1) = 0.0
+    
 ! Forward calculation of tri-diagonal system  # VBA algorithm from Wikipedia
-
-    do k = 3, km
-    
-      bet   = aa(k) / bb(k-1)
-      bb(k) = bb(k) - bet * cc(k-1)
-      dd(k) = dd(k) - bet * dd(k-1)
-         
-    enddo
-
-! Solve for the last value of matrix
-        
-      wE(km)   = dd(km) / bb(km)
-
-! Do the back substition, result is wE on zone edges.
-
-    do k = km-1, 2, -1
-
-      wE(k) = (dd(k) - cc(k) * wE(k+1)) / bb(k)
-        
-    enddo
-    
-! Compute cell centered tridiagonal coefficients 
-
-    do k = 1, km
-    
-      Ak(k) = dt * gama * (pm2(k)+pp(k)) / dz1(k) 
-        
-    enddo
-
-! Compute edge centered tridiagonal coefficients and RHS
 
     do k = 2, km
-
-      Ck(k) = 2.0 * dt / (dm2(k) + dm2(k-1))
-    
-      aa(k) = Ak(k-1)*Ck(k)
-        
-      cc(k) = Ak(k  )*Ck(k)
-    
-      dd(k) = wE(k) + Ck(k) * (pp(k) - pp(k-1))
-        
-      bb(k) = 1.0 - cc(k) - aa(k)
-
-    enddo    
-
-! Boundary value calc for forward tri-diagonal solution
-
-   dd(2)  = dd(2)  - 0.0 * Ck(2)    ! this includes the lower bc for w zero here
-    
-   dd(km) = dd(km) - 0.0 * Ck(km)   ! this includes the lower bc for ws  (zero here)
-  
-! Forward calculation of tri-diagonal system  # VBA algorithm from Wikipedia
-
-    do k = 3, km
     
       bet   = aa(k) / bb(k-1)
       bb(k) = bb(k) - bet * cc(k-1)
@@ -120,7 +75,7 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
 ! Solve for the last value of matrix
         
       wE(km)   = dd(km) / bb(km)
-        
+
 ! Do the back substition, result is wE on zone edges.
 
     do k = km-1, 2, -1
@@ -128,34 +83,21 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
       wE(k) = (dd(k) - cc(k) * wE(k+1)) / bb(k)
         
     enddo
+    
+    wE(1) = wE(2)
+    
+! Store off the edge values
+
+    do k = 1, km+1
+    
+      wES(k) = wE(k)
         
-! Solve for new perturbation pressure
-
-    do k = 1, km
-
-      pp(k)  = pp(k) - Ak(k) * (wE(k+1) - wE(k))
-
-    enddo
+    enddo 
     
-! Use new pp at cell centers to get new dz's
+! New solver create nh-pe values for later update.
 
-    do k = 1, km
-    
-      dz2(k) = -dm2(k)*rgas*pt2(k)*exp(capa1*log(pp(k)+pm2(k)))
+! Create PE's from original code....
 
-    enddo
-    
-! Need to generate new w at cell centers...use the time tendency of dz
-
-    do k = 1,km
-
-     !w2(k) = w1(k) - rdt * (dz2(k) - dz1(k))
-     w2(k) = 0.5*(wE(k) + wE(k+1))
-        
-    enddo
-    
-! Edge code from original nh_utils/SIM1
-    
     do k=1,km-1
       g_rat(k) = dm2(k)/dm2(k+1)
          bb(k) = 2.*(1.+g_rat(k))
@@ -182,13 +124,147 @@ subroutine SIM_W_1D(dt, km, rgas, gama, gm2, cp2, kappa, pe, dm2, pm2, pem, w2, 
        pe(k) = pe(k) - gam(k)*pe(k+1)
     enddo
     
-! Retrieve edge pressures
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Now start the implicit solver
 
-!  pe(1) = 0.0
+! Compute cell centered tridiagonal coefficients 
 
- !   do k = 1,km
-      
- !     
- !   enddo
+    do k = 1, km
     
- end subroutine SIM_W_1D
+      Ak(k) = dt * gama * (pm2(k)+pp(k)) / dz1(k) 
+        
+    enddo
+
+! Compute edge centered tridiagonal coefficients and RHS
+
+    do k = 2, km
+
+      Ck(k) = 2.0 * dt / (dm2(k) + dm2(k-1))
+    
+      aa(k) = Ak(k-1)*Ck(k)
+        
+      cc(k) = Ak(k  )*Ck(k)
+    
+      dd(k) = wE(k) + Ck(k) * (pp(k) - pp(k-1))
+        
+      bb(k) = 1.0 - cc(k) - aa(k)
+
+    enddo    
+
+! Boundary value calc for forward tri-diagonal solution
+
+! von Neuman conditions
+
+   bb(2) = 1 - aa(2)
+    
+   dd(km) = dd(km) - 0.0 * Ck(km)   ! this includes the lower bc for ws  (zero here)
+  
+! Forward calculation of tri-diagonal system  # VBA algorithm from Wikipedia
+
+    do k = 3, km
+    
+      bet   = aa(k) / bb(k-1)
+      bb(k) = bb(k) - bet * cc(k-1)
+      dd(k) = dd(k) - bet * dd(k-1)
+         
+    enddo
+
+! Solve for the last value of matrix
+        
+      wE(km)   = dd(km) / bb(km)
+        
+      wE(km+1) = 0.0
+        
+! Do the back substition, result is wE on zone edges.
+
+    do k = km-1, 2, -1
+
+      wE(k) = (dd(k) - cc(k) * wE(k+1)) / bb(k)
+        
+    enddo
+    
+    wE(1) = wE(2)
+        
+! Solve for new perturbation pressure at the cell centers.
+
+    do k = 1, km
+
+      pp(k)  = pp(k) - Ak(k) * (wE(k+1) - wE(k))
+
+    enddo
+    
+! Use new pp at cell centers to get new dz's
+
+    do k = 1, km
+    
+      dz2(k) = -dm2(k)*rgas*pt2(k)*exp(capa1*log(pp(k)+pm2(k)))
+
+    enddo
+    
+! Need to generate new w at cell centers...use the time tendency of dz
+
+    w2(km) = ( wE(km) + 2.*wE(km+1) )*r3
+    
+    do k = km-1,1,-1
+
+     !w2(k) = 0.5*(wE(k) + wE(k+1))
+        
+     w2(k) = (wE(k) + bb(k)*wE(k+1) + g_rat(k)*wE(k+2))*r3 - g_rat(k)*w2(k+1)
+        
+    enddo
+    
+    pe(1) = 0.0
+    
+    if( pe_compute .eq. 1 ) THEN
+    
+      do k=1,km
+          pe(k+1) = pe(k) + dm2(k)*(w2(k)-w1(k))*rdt
+      enddo
+
+    ENDIF 
+    
+    IF( pe_compute .eq. 2 ) THEN
+    
+! Edge code from original nh_utils/SIM1
+
+        do k=1,km-1
+          g_rat(k) = dm2(k)/dm2(k+1)
+             bb(k) = 2.*(1.+g_rat(k))
+             dd(k) = 3.*(pp(k) + g_rat(k)*pp(k+1))
+        enddo
+
+        bet = bb(1)
+        pp(1) = 0.
+        pp(2) = dd(1) / bet
+        bb(km) = 2.
+        dd(km) = 3.*pp(km)
+
+! Forward calculation of tri-diagonal system
+
+        do k=2,km
+          gam(k) =  g_rat(k-1) / bet
+          bet    =  bb(k) - gam(k)
+          pe(k+1) = (dd(k) - pe(k) ) / bet
+        enddo
+
+! Do the back substition, result is pp on zone edges.
+
+        do k=km, 2, -1
+           pe(k) = pe(k) - gam(k)*pe(k+1)
+        enddo
+            
+    ENDIF
+
+   IF( pe_compute .eq. 3 ) THEN
+    
+! Solve for new perturbation pressure at the cell EDGES
+
+     do k = 2, km
+
+       pe(k)  = pe(k) - 0.5*(Ak(k)+Ak(k-1)) * (w2(k) - w2(k-1))
+
+     enddo
+            
+    ENDIF
+    
+end subroutine SIM_W_1D
