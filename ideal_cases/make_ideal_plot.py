067159da787e0048ca42d4adad33e765b2a83799
louis.wicker <Louis.Wicker@noaa.gov>
2023-01-06 17:56:54 -0600

    updated for AMS

--- ideal_cases/make_ideal_plot.py
+++ ideal_cases/make_ideal_plot.py
@@ -3,25 +3,24 @@ import glob
 import os
 import numpy as np
 from netCDF4 import Dataset
-import sharppy.sharptab.profile as profile
-import sharppy.sharptab.params as params
+# import sharppy.sharptab.profile as profile
+# import sharppy.sharptab.params as params
 import datetime
-from test_wof_sounding_plot import plot_wof
-import sharppy.sharptab.utils as utils
-import sharppy.sharptab.thermo as thermo
+#from test_wof_sounding_plot import plot_wof
+# import sharppy.sharptab.utils as utils
+# import sharppy.sharptab.thermo as thermo
 import argparse
 import time
 import multiprocessing
 import sys
 import warnings
-from analsnd import analsnd_mod
+# from analsnd import analsnd_mod
 from idealized_sounding_fcts import mccaul_weisman
 
 warnings.filterwarnings("ignore",message="converting a masked element to nan",append=True)
 warnings.filterwarnings("ignore",message="overflow encountered in multiply",append=True)
 warnings.simplefilter(action="ignore",category=RuntimeWarning)
 warnings.simplefilter(action="ignore",category=UserWarning)
-
 
 grav = 9.806
 Cp   = 1004.
@@ -36,7 +35,6 @@ def sounding(gz, dz, theta_sfc = 300., qv_sfc = 14.):
     theta_trop = 343.
     temp_trop  = 213.
 
-    
     # compute theta profile
     theta = 0.0*gz
     t1    = theta_sfc + (theta_trop - theta_sfc)*((gz/z_trop)**1.25)
@@ -45,8 +43,6 @@ def sounding(gz, dz, theta_sfc = 300., qv_sfc = 14.):
     
     # compute rh profile
     rh    = np.where(gz <= z_trop, 1.0 - 0.75*((gz/z_trop)**1.25), 0.25)
-    
-    
     
     # integrate hydrostatic eq
     pi    = 0.0*gz
@@ -105,7 +101,8 @@ def write_2_file(p0, th0, q0, z, theta, qv, u, v, filename='wk.sounding'):
             f.write("%12.4f  %12.4f  %12.4f  %12.4f  %12.4f\n" % (z[k], theta[k], 1000.*qv[k], u[k], v[k]))
         
     f.close()
-        
+
+######################################################################################################
 nz = 300
 ztop = 30000.
 gze = zgrid(nz+1, height = ztop, stag=True)
@@ -115,20 +112,24 @@ dz  = gze[1:] - gze[:-1]
 zcape = 12.0E3
 ztrop = 12.0E3
 
+
+CAPEs =(1000.,)
 CAPEs =(1000, 1500, 2000, 2500, 3000, 3500)
-#CAPE = 2000.
 qvs = (11., 12., 13., 14., 15., 16.)
 m = 1.8
-us1 = 12.5
+us1 = 18.
 pblthe = 343.0
+ROCP   = 0.28571426 # from sharppy
+
 #def mccaul_weisman(z, E=2000.0, m=2.2, H=12500.0, z_trop=12000.0, RH_min=0.1, p_sfc=1e5,
 #                   T_sfc=300.0, thetae_pbl=335.0, pbl_lapse=0.009, crit_lapse=0.0095, 
 #                   pbl_depth=None, lr=0.0001):
 
 for i, CAPE in enumerate(CAPEs):
   fname_plot = 'poly_sounding_C%d'%(CAPE)
-  fname_out = 'poly_input_sounding_C%d'%(CAPE)
+  fname_out = 'poly_input_sounding_C%d_%2.2d.snd'%(CAPE,us1)
   profile_mw = mccaul_weisman(hgt,CAPE,m,zcape,ztrop,thetae_pbl=pblthe,RH_min=0.25,pbl_lapse = 0.008)
+
 #Returns     
 #        thermo_prof : pd.DataFrame
 #        MW01 sounding
@@ -140,33 +141,35 @@ for i, CAPE in enumerate(CAPEs):
   z = profile_mw['z']
   p = profile_mw['prs']
   t = profile_mw['T'] 
-  th = thermo.theta(p/100.,t-273.15)+273.15
-  qv = profile_mw['qv']
+  th  = t * (1.0e5/p)**ROCP
+    
+  qv  = profile_mw['qv']
   tdc = profile_mw['Td'] - 273.15
-  tc = t - 273.15
+  tc  = t - 273.15
 
 #fname_plot = 'sounding_qv%d'%(q)
 #fname_out = 'input_sounding_q%d'%(q)
 #th,tc,qv,p,tdc = sounding(hgt,dz,qv_sfc=q)
 #tdc = td #thermo.temp_at_mixrat(qv*1000., p/100.)
 #t = thermo.theta(1000.,th, p/100.)
-  u,v = shear(hgt)
 
-  ukt = u*1.94384
-  vkt = v*1.94384
-  omega = np.full(v.shape,0.0)
+  u,v = shear(hgt, shear=us1)
 
-  start_date = '201905202100'
-  idateobj = datetime.datetime.strptime(start_date,'%Y%m%d%H%M')
-  vdateobj = idateobj 
-  member_profs = np.empty(1, dtype=object)
-  prof = profile.create_profile(profile='convective', pres=p/100., hght=hgt, tmpc=tc, \
-					dwpc=tdc, u=u, v=v, omega=omega, missing=-9999, latitude=35.0, strictQC=False,date=vdateobj )
-  prof.pw = params.precip_water(prof)
-  member_profs[0] = prof
-  members = {'hght': hgt, 'pres': p, 'tmpc': tc, 'dwpc': tdc, 'u': ukt, 'v': vkt, 'member_profs': member_profs}
+#   ukt = u*1.94384
+#   vkt = v*1.94384
+#   omega = np.full(v.shape,0.0)
 
-  plot_wof(prof, members, fname_plot , 0.0, 0.0, idateobj, vdateobj,x_pts=(0,0),y_pts=(0,0))
+#   start_date = '201905202100'
+#   idateobj = datetime.datetime.strptime(start_date,'%Y%m%d%H%M')
+#   vdateobj = idateobj 
+#   member_profs = np.empty(1, dtype=object)
+#   prof = profile.create_profile(profile='convective', pres=p/100., hght=hgt, tmpc=tc, \
+# 					dwpc=tdc, u=u, v=v, omega=omega, missing=-9999, latitude=35.0, strictQC=False,date=vdateobj )
+#   prof.pw = params.precip_water(prof)
+#   member_profs[0] = prof
+#   members = {'hght': hgt, 'pres': p, 'tmpc': tc, 'dwpc': tdc, 'u': ukt, 'v': vkt, 'member_profs': member_profs}
+
+#plot_wof(prof, members, fname_plot , 0.0, 0.0, idateobj, vdateobj,x_pts=(0,0),y_pts=(0,0))
   write_2_file(p[0]/100.,th[0],qv[0]*1000.,hgt,th,qv,u,v,filename=fname_out)
     
     
