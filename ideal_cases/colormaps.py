751c7ddcf86f7a469f4ae18affe6755d08cd97b3
louis.wicker <Louis.Wicker@noaa.gov>
2023-03-20 19:48:07 -0500

    reorg of soundings

---
+++ ideal_cases/colormaps.py
@@ -0,0 +1,30 @@
+import matplotlib as mpl
+
+def radar_colormap():
+    nws_reflectivity_colors = [
+    "#646464", # ND
+    "#ccffff", # -30
+    "#cc99cc", # -25
+    "#996699", # -20
+    "#663366", # -15
+    "#cccc99", # -10
+    "#999966", # -5
+    "#646464", # 0
+    "#04e9e7", # 5
+    "#019ff4", # 10
+    "#0300f4", # 15
+    "#02fd02", # 20
+    "#01c501", # 25
+    "#008e00", # 30
+    "#fdf802", # 35
+    "#e5bc00", # 40
+    "#fd9500", # 45
+    "#fd0000", # 50
+    "#d40000", # 55
+    "#bc0000", # 60
+    "#f800fd", # 65
+    "#9854c6", # 70
+    "#fdfdfd" # 75
+    ]
+
+    return mpl.colors.ListedColormap(nws_reflectivity_colors)
