3a1ee01dc0823c12ca61da0a937bf19c51512870
louis.wicker <Louis.Wicker@noaa.gov>
2023-04-19 16:32:56 -0500

    Added new scripts to auto process objects and precipitation files for plotting

---
+++ ideal_cases/squall_precip.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import sys
+
+import numpy as np
+import matplotlib as mlab
+import matplotlib.pyplot as plt
+import xarray as xr
+import os as os
+import glob
+import matplotlib.gridspec as gridspec
+import metpy.calc as mpcalc
+import pickle
+
+import tools
+
+from analysis_tools import read_solo_fields, read_wrf_fields, read_cm1_fields
+
+
+w_thresh = 5.0
+cref_thresh = 35.
+percent = None
+
+zhgts = 250. + 250.*np.arange(100)
+
+dirs    = {
+           "solo": "/work/wicker/Odin_scr/solo",
+           "wrf": "/work/wicker/WRF/WRF/test/em_quarter_ss",
+           "cm1": "/work/wicker/Odin_scr/cm1r20.3/run",
+          }
+
+profile_dir = "profiles"
+
+run      = {"solo": "squall_1km", "wrf": "squall_1km", "cm1": "squall_1km"}
+run      = {"solo": "squall_1km"}
+allcape  = ("C2000", "C3500")
+allshear = ( "06", "18" )
+
+solo  = {}
+cm1   = {}
+wrf   = {}
+
+for key in run:
+
+    field = {}
+
+    for shear in allshear:
+        for cape in allcape:
+        
+            label = "%s_%s" % (cape, shear)
+
+
+            file = str(os.path.join(dirs[key], "%s_%s" % (run[key], label)))
+
+            if key == 'solo':
+                solo[label] = read_solo_fields(file, file_pattern=None, vars=['accum_prec'])
+                
+            if key == 'wrf':
+
+                wrf[label] = read_wrf_fields(file, file_pattern=None, vars=['accum_prec'])
+
+            if key == 'cm1':
+                cm1[label] = read_cm1_fields(file, file_pattern=None, vars=['accum_prec'])
+                
+for key in run.keys():
+    
+    if key == 'solo':
+        with open('precip/solo_%s_accum_prec.pkl' % run['solo'], 'wb') as handle:
+            pickle.dump(solo, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    if key == 'wrf':
+        with open('precip/wrf_%s_accum_prec.pkl' % run['wrf'], 'wb') as handle:
+            pickle.dump(wrf, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    if key == 'cm1':
+        with open('precip/cm1_%s_accum_prec.pkl' % run['cm1'], 'wb') as handle:
+            pickle.dump(cm1, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print("\n Squall_precip wrote pickled file:  %s out!\n" % \
+            ('%s/%s_%s_35dbz_profiles.pkl' % (profile_dir, key, run[key])))
+
